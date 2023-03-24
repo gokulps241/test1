c7493bd34b4ea0605554faad80fd20ce3b198964
gokulps241 <128774493+gokulps241@users.noreply.github.com>
2023-03-24 11:36:36 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,10 +1,8 @@
 class simple{  
     public class Main {
   public static void main(String[] args) {
-    int i = 0;
-    while (i < 5) {
+    for (int i = 0; i < 5; i++) {
       System.out.println(i);
-      i++;
     }  
   }
 }
